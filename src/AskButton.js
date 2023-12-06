6735ece63bd3b422f148c6f6ac1670e3bdb53b2e
Igor Yegorov <m2127299045@gmail.com>
2023-12-05 17:59:02 -0800

    check to see if the 'answer' is a string

--- src/AskButton.js
+++ src/AskButton.js
@@ -5,25 +5,29 @@ import React, { useState } from 'react';
 const AskButton = () => {
   const [response, setResponse] = useState('');
 
-  const handleAskClick = async () => {
-    try {
-      // Note: You may need to adjust this URL based on the actual CORS configuration of the server.
-      const url = 'https://vyvvqaw46b643cmqiohh4abq3q0sglbb.lambda-url.us-west-2.on.aws';
-      const result = await fetch(url);
+const handleAskClick = async () => {
+  try {
+    const url = 'https://vyvvqaw46b643cmqiohh4abq3q0sglbb.lambda-url.us-west-2.on.aws';
+    const result = await fetch(url);
 
-      // Check if the response status is OK (200)
-      if (result.ok) {
-        const jsonResult = await result.json();
-        // Assuming the JSON output has an 'Answer' property
-        const answer = jsonResult.Answer;
-        setResponse(answer);
-      } else {
-        console.error('Failed to fetch data. Status:', result.status);
-      }
-    } catch (error) {
-      console.error('Error fetching data:', error);
+    if (result.ok) {
+      const jsonResult = await result.json();
+
+      // Assuming the JSON output has an 'Answer' property
+      const answer = jsonResult.Answer;
+
+      // Check if 'answer' is a string, if not, convert it to a string
+      const responseText = typeof answer === 'string' ? answer : JSON.stringify(answer);
+
+      setResponse(responseText);
+    } else {
+      console.error('Failed to fetch data. Status:', result.status);
     }
-  };
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
 
   return (
     <div>
