b79292ae3ea8d4c67a308ad805b4e380c4184d77
Igor Yegorov <m2127299045@gmail.com>
2024-01-16 18:22:50 -0800

    dummy

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import AskButton from './AskButton';
 const App = () => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh' }}>
-      Deep Thought, the second greatest computer ever, is here.
+      Deep Thought, the second greatest computer ever, is here!
       <AskButton />
     </div>
   );
