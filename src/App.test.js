8031e5937857e2f021b0b48dea00d2c5b3ca5a12
Igor Yegorov <m2127299045@gmail.com>
2024-01-16 18:59:32 -0800

    App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
+// src/App.test.js
+
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  test('renders the message about Deep Thought', () => {
+    render(<App />);
+    const messageElement = screen.getByText(/Deep Thought, the second greatest computer ever, is here./i);
+    expect(messageElement).toBeInTheDocument();
+  });
+
+  test('renders the AskButton component', () => {
+    render(<App />);
+    const buttonElement = screen.getByRole('button');
+    expect(buttonElement).toBeInTheDocument();
+  });
 });
+
