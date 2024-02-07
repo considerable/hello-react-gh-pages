c449872d7e4f6a093e9e38ed6e9551a3d292b97a
Igor Yegorov <m2127299045@gmail.com>
2024-02-07 12:14:21 -0800

    Humility

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ import AskButton from './AskButton';
 const App = () => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-      <h1>Deep Thought, the second greatest computer ever, is here.</h1>
-      <label htmlFor="question">Ask me anything</label>
-      <textarea id="question" name="question" placeholder="Enter your question" spellCheck="true" />
+      Deep Thought, the second greatest computer ever, is here.
       <AskButton />
       <p>&nbsp;</p>
       <p>Want to see how I work? Check out my source code <a href="https://github.com/considerable/hello-react-gh-pages">here</a></p>
