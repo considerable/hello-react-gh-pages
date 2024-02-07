0533108d264542ed59c28507c79e08d8eeaa5f3e
Igor Yegorov <m2127299045@gmail.com>
2024-02-07 10:32:38 -0800

    fit

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import AskButton from './AskButton';
 
 const App = () => {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh' }}>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '64vh' }}>
       Deep Thought, the second greatest computer ever, is here.
       <AskButton />
-      Want to see how I work? Check out my source code <a href="https://github.com/considerable/hello-react-gh-pages">here</a>.
+      Want to see how I work? Check out my source code <a href="https://github.com/considerable/hello-react-gh-pages">here</a>
     </div>
   );
 };
