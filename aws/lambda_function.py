3931f864b623aee0c5313523cbce17ee33b1bd03
considerable <m2127299045@gmail.com>
2026-05-04 20:30:06 -0400

    fix: remove duplicate CORS headers - let Lambda URL config handle CORS

--- aws/lambda_function.py
+++ aws/lambda_function.py
@@ -2,25 +2,9 @@
 import json
 
 def lambda_handler(event, context):
-    # Handle CORS preflight
-    if event.get('requestContext', {}).get('http', {}).get('method') == 'OPTIONS':
-        return {
-            'statusCode': 204,
-            'headers': {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'GET, OPTIONS',
-                'Access-Control-Allow-Headers': 'Content-Type',
-                'Access-Control-Max-Age': '86400'
-            },
-            'body': ''
-        }
-
     return {
         'statusCode': 200,
         'headers': {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET, OPTIONS',
-            'Access-Control-Allow-Headers': 'Content-Type',
             'Content-Type': 'application/json'
         },
         'body': json.dumps(
